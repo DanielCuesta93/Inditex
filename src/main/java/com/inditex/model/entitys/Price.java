e3ecf45b014c06a957a4cf73893be8f7435117db
IBLINETUM\daniel.cuesta <daniel.cuesta@inetum.com>
2025-03-23 20:57:44 +0100

    Pagina principal accesible

--- src/main/java/com/inditex/model/entitys/Price.java
+++ src/main/java/com/inditex/model/entitys/Price.java
@@ -1,7 +1,9 @@
 package com.inditex.model.entitys;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,43 +13,46 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="prices")
+@Table(name="PRICES")
 public class Price implements Serializable{
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 	@Column(name="BRAND_ID")
-	private Long brand_id;
+	private Integer brand_id;
 	@Column(name="START_DATE")
-	private Timestamp start_date;
+	private LocalDateTime  start_date;
 	@Column(name="END_DATE")
-	private Timestamp end_date;
+	private LocalDateTime  end_date;
 	@Column(name="PRICE_LIT")
 	private Integer price_list;
 	@Column(name="PRODUCT_ID")
-	private Long product_id;
+	private Integer product_id;
 	@Column(name="PRIORITY")
 	private Integer priority;
 	@Column(name="PRICE")
-	private Double price;
+	private BigDecimal price;
 	@Column(name="CURR")
 	private String curr;
 	
 	
-	public Long getBrand_id() {
+	public Integer getBrand_id() {
 		return brand_id;
 	}
-	public void setBrand_id(Long brand_id) {
+	public void setBrand_id(Integer brand_id) {
 		this.brand_id = brand_id;
 	}
-	public Timestamp getStart_date() {
+	public LocalDateTime getStart_date() {
 		return start_date;
 	}
-	public void setStart_date(Timestamp start_date) {
+	public void setStart_date(LocalDateTime start_date) {
 		this.start_date = start_date;
 	}
-	public Timestamp getEnd_date() {
+	public LocalDateTime getEnd_date() {
 		return end_date;
 	}
-	public void setEnd_date(Timestamp end_date) {
+	public void setEnd_date(LocalDateTime end_date) {
 		this.end_date = end_date;
 	}
 	public Integer getPrice_list() {
@@ -56,10 +61,10 @@ public class Price implements Serializable{
 	public void setPrice_list(Integer price_list) {
 		this.price_list = price_list;
 	}
-	public Long getProduct_id() {
+	public Integer getProduct_id() {
 		return product_id;
 	}
-	public void setProduct_id(Long product_id) {
+	public void setProduct_id(Integer product_id) {
 		this.product_id = product_id;
 	}
 	public Integer getPriority() {
@@ -68,10 +73,10 @@ public class Price implements Serializable{
 	public void setPriority(Integer priority) {
 		this.priority = priority;
 	}
-	public Double getPrice() {
+	public BigDecimal getPrice() {
 		return price;
 	}
-	public void setPrice(Double price) {
+	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
 	public String getCurr() {
--- src/main/java/com/inditex/rest/PriceController.java
+++ src/main/java/com/inditex/rest/PriceController.java
@@ -1,25 +1,17 @@
 package com.inditex.rest;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.inditex.model.entitys.Price;
-import com.inditex.model.service.inter.IPriceService;
 
-@RestController
+@Controller
 @RequestMapping("/prices")
 public class PriceController {
-	
-	@Autowired
-	private IPriceService priceService;
-	
-	@GetMapping
-	public List<Price> getPrices(){
-		return priceService.getPrice();
-	}
 
+    @GetMapping
+    public String prices(){   	
+    	return "prices-view";
+    }
 }
